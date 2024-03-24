8feddfeadb445ac5b8ac33fcf76a5b543fdf3f8b
ahirsch-github <anja_hirsch@t-online.de>
2024-03-24 12:35:05 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'muell-o-mat',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
